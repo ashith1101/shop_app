b8357fba7d5390fea4519261be8ad0f9d063511b
Ashith Shetty <ashithdshetty1101@gmail.com>
2023-10-31 01:16:37 +0530

    toggle feature

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,41 +1,47 @@
 import 'package:flutter/material.dart';
 import '../screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  const ProductItem({
-    Key? key,
-    required this.id,
-    required this.title,
-    required this.imageUrl,
-  }) : super(key: key);
+  // const ProductItem({
+  //   Key? key,
+  //   required this.id,
+  //   required this.title,
+  //   required this.imageUrl,
+  // }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return GridTile(
       child: GestureDetector(
         onTap: () => {
           Navigator.of(context).pushNamed(
             ProductDetailScreen.routeName,
-            arguments: id,
+            arguments: product.id,
           ),
         },
-        child: Image.network(imageUrl, fit: BoxFit.cover),
+        child: Image.network(product.imageUrl, fit: BoxFit.cover),
       ),
       footer: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () => {},
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            onPressed: () => {
+              product.toggleFavoriteStatus(),
+            },
             color: Theme.of(context).hintColor,
           ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -4,18 +4,20 @@ import 'package:provider/provider.dart';
 import '../providers/products.dart';
 
 class ProductsGrid extends StatelessWidget {
-   
   @override
   Widget build(BuildContext context) {
-    final productsData=  Provider.of<Products>(context);
+    final productsData = Provider.of<Products>(context);
     final products = productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(16),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ProductItem(
-          id: products[i].id,
-          title: products[i].title,
-          imageUrl: products[i].imageUrl
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        create: (c) => products[i],
+        child: ProductItem(
+            // id: products[i].id,
+            // title: products[i].title,
+            // imageUrl: products[i].imageUrl),
+            ),
       ),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
